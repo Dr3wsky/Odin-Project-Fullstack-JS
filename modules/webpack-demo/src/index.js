6ef192170626fb16bde07b654bc4bb313047d416
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-09 12:42:52 -0600

    build with css loader

--- modules/webpack-demo/src/index.js
+++ modules/webpack-demo/src/index.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
+  const element2 = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.textContent = myName('Cody');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element2.textContent = myName('Cody');
+  element.classList.add('hello');
   
   return element;
 }
