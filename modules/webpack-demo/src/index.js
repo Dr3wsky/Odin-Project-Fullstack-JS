0058671f54ae91aca88be09e450e0e391331483d
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-09 13:01:21 -0600

    added image asset management

--- modules/webpack-demo/src/index.js
+++ modules/webpack-demo/src/index.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
-import myName from './myName';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
-  const element2 = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element2.textContent = myName('Cody');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  
+  element.appendChild(myIcon);
   
   return element;
 }
