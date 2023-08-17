1c217b0ed24dc4d0a4fb44babdab35aa0c3e3005
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-17 13:10:23 -0600

    splitting JS into separate file

---
+++ APIs/gifMakerAPI/index.js
@@ -0,0 +1,11 @@
+const img = document.querySelector('img');
+const imgSearch = 'zebra'
+const apiBase = 'https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=';
+fetch(apiBase.concat(imgSearch), {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    img.src =  response.data.images.original.url;
+   
+  });
