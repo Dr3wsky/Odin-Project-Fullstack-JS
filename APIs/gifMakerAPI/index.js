3d1ab546b917f4b94970133cfbff5a31f368ec7f
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-18 12:16:59 -0600

    finish core gif search functionality

--- APIs/gifMakerAPI/index.js
+++ APIs/gifMakerAPI/index.js
@@ -1,10 +1,24 @@
+// DOM handling
 const img = document.querySelector('img');
+const searchBtn = document.getElementById('search-btn')
 const imgSearch = 'zebra'
 const apiBase = 'https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=';
-fetch(apiBase.concat(imgSearch), {mode: 'cors'})
+
+// Event handler
+searchBtn.onclick = searchGif;
+
+// Functions to search and return gifs
+function searchGif(img) {
+const gifInput = document.getElementById("gif-search").value;
+return img.src = fetchGif(apiBase, gifInput);
+}
+
+function fetchGif(apiBase, gifInput){
+fetch(apiBase.concat(gifInput), {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
-    img.src =  response.data.images.original.url;
+img.src =  response.data.images.original.url;
       });
+}
