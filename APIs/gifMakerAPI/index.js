3573f3069749a98b763173d2cb0390231aaf8bab
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-18 12:57:44 -0600

    added submit on enter

--- APIs/gifMakerAPI/index.js
+++ APIs/gifMakerAPI/index.js
@@ -1,12 +1,21 @@
 // DOM handling
 const img = document.querySelector('img');
 const searchBtn = document.getElementById('search-btn')
-const imgSearch = 'zebra'
+const searchInput = document.getElementById('gif-search')
+const imgDefault = 'start';
 const searchError = document.getElementById("search-error");
 const apiBase = 'https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=';
 
-// Event handler
+
+
+// Event handlers
 searchBtn.onclick = searchGif;
+searchInput.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    searchBtn.click();
+  };
+}); 
 
 // Functions to search and return gifs
 function searchGif(img) {
@@ -26,5 +35,6 @@ fetch(apiBase.concat(gifInput), {mode: 'cors'})
   })
   .catch(e => {
     searchError.textContent = "Invalid search, no gifs found. Try again."
+    img.src = '#';
   });
 }
