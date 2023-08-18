1e447948e4fb4e3c4bcde68977e7d34b7aabc368
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-08-18 12:41:03 -0600

    added error message

--- APIs/gifMakerAPI/index.js
+++ APIs/gifMakerAPI/index.js
@@ -2,6 +2,7 @@
 const img = document.querySelector('img');
 const searchBtn = document.getElementById('search-btn')
 const imgSearch = 'zebra'
+const searchError = document.getElementById("search-error");
 const apiBase = 'https://api.giphy.com/v1/gifs/translate?api_key=5USKJ6tsZp7la5EBedWxG0Z9vjybhKFY&s=';
 
 // Event handler
@@ -10,15 +11,20 @@ searchBtn.onclick = searchGif;
 // Functions to search and return gifs
 function searchGif(img) {
 const gifInput = document.getElementById("gif-search").value;
-return img.src = fetchGif(apiBase, gifInput);
+img.src = fetchGif(apiBase, gifInput);
 }
 
 function fetchGif(apiBase, gifInput){
 fetch(apiBase.concat(gifInput), {mode: 'cors'})
   .then(function(response) {
+    console.log(response.json);
     return response.json();
   })
   .then(function(response) {
-img.src =  response.data.images.original.url;
-      });
+    searchError.textContent = "";
+    img.src =  response.data.images.original.url;
+  })
+  .catch(e => {
+    searchError.textContent = "Invalid search, no gifs found. Try again."
+  });
 }
