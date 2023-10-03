afb644a8028a9f4c753d783a01526390ef283ba9
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-03 14:01:28 -0600

    completing linkedList funcs

--- computer-science/linked-lists/index.js
+++ computer-science/linked-lists/index.js
@@ -3,12 +3,39 @@ import LinkedList from "./linkedList.js";
 // Tests
 const linkedList = new LinkedList();
 
+// Populate linkedList
 linkedList.prepend("test1");
 linkedList.append("test2");
 linkedList.append("test3");
-linkedList.prepend("test4");
-console.log(`List is ${linkedList.size()} nodes long`);
-// console.log(linkedList.head());
-// console.log(linkedList.tail());
-console.log(linkedList.at(2));
-console.log(linkedList.at(5));
+
+// Returns size of list
+console.log(linkedList.size()); // 3
+
+// Returns list head and tail
+console.log(linkedList.head()); // return head Node
+console.log(linkedList.tail()); // Node { value: 'test3', nextNode: null }
+
+// Returns node at index value
+console.log(linkedList.at(2)); // Node { value: 'test3', nextNode: null }
+console.log(linkedList.at(4)); // There is no item for this index
+
+// Removes last node and checks if worked
+linkedList.pop();
+console.log(linkedList.at(2)); // There is no item for this index
+
+// Cast list values to string
+console.log(linkedList.toString()); // ( test1 ) -> ( test2 ) -> null
+
+// Find indicies and values of values 
+console.log(linkedList.contains("test1")); // true
+console.log(linkedList.find("test2")); // 1
+
+// Populate list
+linkedList.prepend("test3");
+linkedList.insertAt("test4", 2);
+console.log(linkedList.toString()); // ( test3 ) -> ( test1 ) -> ( test4 ) -> ( test2 ) -> null
+linkedList.insertAt("test5", 8);
+console.log(linkedList.toString()); // ( test3 ) -> ( test1 ) -> ( test4 ) -> ( test2 ) -> ( test5 ) -> null
+linkedList.removeAt(2);
+console.log(linkedList.toString()); // ( test3 ) -> ( test1 ) -> ( test2 ) -> ( test5 ) -> null
+
--- computer-science/linked-lists/linkedList.js
+++ computer-science/linked-lists/linkedList.js
@@ -1,4 +1,3 @@
-import IsGenericDescriptor from "es-abstract/5/IsGenericDescriptor.js";
 import Node from "./node.js";
 
 export default class LinkedList {
@@ -6,6 +5,7 @@ export default class LinkedList {
     this.listHead = null;
   }
 
+  // Add new node at start of list
   prepend(value) {
     let tmp = null;
     if (this.listHead != null) tmp = this.listHead;
@@ -13,6 +13,7 @@ export default class LinkedList {
     this.listHead.nextNode = tmp;
   }
 
+  // Append new node to end of list
   append(value) {
     if (this.listHead == null) this.prepend(value);
     else {
@@ -24,6 +25,7 @@ export default class LinkedList {
     }
   }
 
+  // Returns size of list
   size() {
     let tmp = this.listHead;
     let count = 0;
@@ -34,16 +36,19 @@ export default class LinkedList {
     return count;
   }
 
+  // Returns head of list
   head() {
     return this.listHead;
   }
 
+  // Returns tail of list
   tail() { 
     let tmp = this.listHead;
     while (tmp.nextNode != null) tmp = tmp.nextNode;
     return tmp;
   }
 
+  // returns node at desired index
   at(index) {
     let tmp = this.listHead;
     for (let i = 0; i<index; i++) {
@@ -53,5 +58,80 @@ export default class LinkedList {
     return tmp;
   }
 
+  // removes last node of list
+  pop() { 
+    let cur = this.listHead;
+    let prev = null;
+    while (cur.nextNode != null) {
+      prev = cur;
+      cur = cur.nextNode;
+    }
+    prev.nextNode = null;
+  }
+
+  // Checks if list contains value
+  contains(value) {
+    let tmp = this.listHead;
+    while (tmp != null) {
+      if (tmp.value === value) return true
+      tmp = tmp.nextNode;
+    }
+    return false;
+  }
+
+  // returns index of value in list
+  find(value) {
+    let tmp = this.listHead;
+    let index = 0;
+    while (tmp != null) {
+      if (tmp.value === value) return index;
+      tmp = tmp.nextNode;
+      index++;
+    }
+    return null;
+  }
+
+  // Cast entire linked list to string
+  toString() {
+    let tmp = this.listHead;
+    let strList = "";
+    while (tmp != null) {
+      strList += `( ${tmp.value} ) -> `;
+      tmp = tmp.nextNode;
+    }
+    return (strList += "null");
+  }
+
+  // Inserts new node in middle of list at desired index
+  insertAt(value, index) {
+    if (this.listHead == null) this.prepend(value)
+    else {
+  let cur = this.listHead;
+  let prev = null;
+      for (let i = 0; i < index; i++) {
+        prev = cur;
+        cur = cur.nextNode;
+        if (cur == null) break; // if index is bigger than list, add node at end of list 
+    }
+    let tmp = new Node(value);
+    prev.nextNode = tmp;
+    tmp.nextNode = cur;
+    }
+  }
+
+    // Removes node in middle of list at desired index
+    removeAt(index) {
+      if (this.listHead == null) return "List is already empty"
+      else {
+        let cur = this.listHead;
+        let prev = null;
+        for (let i = 0; i < index; i++) {
+          prev = cur;
+          cur = cur.nextNode;
+          if (cur == null) return "The list is shorter than this index";
+        }
+        prev.nextNode = cur.nextNode; // Gives current node pointer to previous node pointer, thus dropping the current node from list 
+      }
+    }
 
 }
