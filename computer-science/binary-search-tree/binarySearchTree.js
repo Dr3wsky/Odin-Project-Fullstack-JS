237507f69ee89dfad95d9551b40eb9cabc675d55
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-04 02:57:39 -0600

    folder struct and file templates

---
+++ computer-science/binary-search-tree/binarySearchTree.js
@@ -0,0 +1,36 @@
+import Node from "./node.js";
+
+export default class BinarySearchTree {
+
+  constructor() {
+    this.root = root;
+  }
+
+  createBST(arr) {
+    let start = arr[0];
+    let end = arr[arr.length];
+
+    if (arr[0] > arr[arr.length]) return null;
+
+    let mid = Math.flloor(arr.length/2);
+    let node = newNode(arr[mid]);
+    node.left = createBST(arr.slice(0, mid));
+    node.right = createBST(arr.slice(mid, arr.lenght));
+    return node;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+  
+};
+
+}
