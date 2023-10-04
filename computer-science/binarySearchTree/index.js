a785b4b741625091392247e99d8a9c974e4a7e8e
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-04 02:58:15 -0600

    updating files

---
+++ computer-science/binarySearchTree/index.js
@@ -0,0 +1,9 @@
+import BinarySearchTree from "./binarySearchTree.js";
+
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+// Tests
+const bst = new BinarySearchTree(testArr);
+
+BinarySearchTree.prettyPrint()
+
