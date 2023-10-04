b3f751fdbe5f483f442cb6751c7a092e579812f2
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-04 03:37:01 -0600

    core funds

--- computer-science/binary-search-tree/binarySearchTree.js
+++ computer-science/binary-search-tree/binarySearchTree.js
@@ -2,35 +2,37 @@ import Node from "./node.js";
 
 export default class Tree {
 
-  constructor() {
+  constructor(arr) {
     this.root = this.buildTree(arr);
   }
 
-  sortedArrayToBST(arr) {
-    let start = arr[0];
-    let end = arr[arr.length];
+  buildTree(arr) {
+    const root = this.sortedArrayToBST(arr, 0, arr.length) 
+    return root;
+  }
 
-    if (arr[0] > arr[arr.length]) return null;
+  sortedArrayToBST(arr, start, end) {
+    if (start > end) return null;
 
-    let mid = Math.flloor(arr.length/2);
-    let node = new Node(arr[mid]);
-    node.left = createBST(arr.slice(0, mid));
-    node.right = createBST(arr.slice(mid, arr.lenght));
+    const mid = parseInt((start + end) / 2, 10);
+    const node = new Node(arr[mid]);
+    node.left = sortedArrayToBST(arr, start, mid - 1);
+    node.right = sortedArrayToBST(arr, mid + 1, end);
     return node;
   }
 
-  prettyPrint(node, prefix = "", isLeft = true) {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
+  // prettyPrint(node, prefix = "", isLeft = true) {
+  //   if (node === null) {
+  //     return;
+  //   }
+  //   if (node.right !== null) {
+  //     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  //   }
+  //   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  //   if (node.left !== null) {
+  //     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  //   }
   
-  }
+  // }
 
 }
