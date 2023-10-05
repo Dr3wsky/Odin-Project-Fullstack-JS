44727a0312353c5c5f1c74962adf9c63b9f92b14
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-05 14:02:14 -0600

    finishing delete and helper funcs

--- computer-science/binary-search-tree/binarySearchTree.js
+++ computer-science/binary-search-tree/binarySearchTree.js
@@ -38,27 +38,39 @@ export default class Tree {
   }
 
   insert(value, node = this.root) {
+    // Do nothing if value exists
+    if (value === node.value) return;
+
     // Inserts as new node (leaf) if at null value (end of branch)
     if (node == null) {
       node = new Node(value);
       return node;
     }
-
-    // Do nothing if value exists
-    if (value === node.value) return;
     
     // Traverse tree to end of leaf and insert value as new node
     if (value < node.value) {
       node.left = this.insert(value, node.left);
-    } else {
+    } else if (value > node.value) {
       node.right = this.insert(value, node.right);
     }
     return node;
   }
 
   delete(value, node = this.root) {
+    // Returns if no node with value found
+    if (node === null) return node;
+    
+    // Traverse tree to find node with desired value, delete with helper method if value found
+    if (value == node.value) {
+      node = this.#removeNodeHelper(node);
+    } else if (value < node.value) {
+      node.left = this.delete(value, node.left)
+    } else {
+      node.right = this.delete(value, node.right);
+    }
+    return node;
+    }
 
-  }
 
   // Prints visual depiction of binary tree
   prettyPrint(node, prefix = "", isLeft = true) {
@@ -71,6 +83,33 @@ export default class Tree {
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+
+  //Private Methods
+  
+  #inOrderSuccessor(node) {
+    let tmpNode = node;
+    while(tmpNode.left) {
+      tmpNode = tmpNode.left;
+    }
+    return tmpNode;
+  }
+
+  #removeNodeHelper(node) {
+    // If two children, bring up lowest right node to replace deleted, and reorder right nodes to follow
+    if (node.left && node.right) {
+      const successorNode = this.#inOrderSuccessor(node.right);
+      node.value = successorNode.value;
+      node.right = this.delete(successorNode.value, node.right);
+      return node;
+    }
+    // If one child, replace deleted node with child
+    else {
+      const replacementNode = node.left || node.right;
+      node = null;
+      return replacementNode;
     }
   }
 
--- computer-science/binary-search-tree/index.js
+++ computer-science/binary-search-tree/index.js
@@ -2,14 +2,16 @@ import Tree from "./binarySearchTree.js";
 
 const simpleArr = [1, 4, 25, 66, 98, 150];
 
-const testArr = [1, 556, 27, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testArr = [1, 556, 556, 27, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 // Tests
 const binaryTree = new Tree(testArr);
 
 console.log(binaryTree)
 binaryTree.prettyPrint(binaryTree.root)
+console.log("-------------------------------------------")
 binaryTree.insert(34);
 binaryTree.prettyPrint(binaryTree.root)
-
-
+console.log("-------------------------------------------")
+binaryTree.delete(4);
+binaryTree.prettyPrint(binaryTree.root)
