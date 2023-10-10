ed86ec3480fe5404fbd6281952c468702f7e3793
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-10 13:54:40 -0600

    adding private function as levelOrder param

--- computer-science/binary-search-tree/index.js
+++ computer-science/binary-search-tree/index.js
@@ -9,15 +9,16 @@ const binaryTree = new Tree(testArr);
 binaryTree.prettyPrint(binaryTree.root) // Prints binary search tree with no duplicate values
 console.log("-------------------------------------------")
 binaryTree.insert(36);
-binaryTree.prettyPrint(binaryTree.root) // Adds 34 to leaf of existing tree
+binaryTree.prettyPrint(binaryTree.root) // Adds 36 to leaf of existing tree
 console.log("-------------------------------------------")
 binaryTree.delete(4);
-binaryTree.prettyPrint(binaryTree.root) // Removes node 5 and reorders children
+binaryTree.prettyPrint(binaryTree.root) // Removes node 4 and reorders children
 console.log("-------------------------------------------")
 console.log(binaryTree.find(27)) // Node {value: 34, left: null, right: node}
 console.log(binaryTree.find(9)) // Node {value: 34, left: Node, right: Node}
 console.log(binaryTree.depth(7)) // 2
 console.log("-------------------------------------------")
-binaryTree.levelOrder(); // Returns array of tree values traverseed in level order [ ]
-console.log('halt')
+binaryTree.levelOrder(); // Returns array of tree values traverseed in level order 
+console.log(binaryTree.levelOrderTraversed) // [9, 5, 67, 1, 7, 23, 556, 3, 8, 27, 324, 6345, 36]
+
  
