526faca3a8a20212365b7be62cbd8a90e993789a
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-10 15:37:36 -0600

    making ordered lists as obj properties

--- computer-science/binary-search-tree/index.js
+++ computer-science/binary-search-tree/index.js
@@ -21,9 +21,13 @@ console.log(binaryTree.find(9)) // Node {value: 34, left: Node, right: Node}
 console.log(`Node depth is ${binaryTree.depth(7)}`) // 2
 console.log("-------------------------------------------")
 binaryTree.levelOrder(); // Returns array of tree values traverseed in level order 
-console.log(`Tree travesed in level order is: ${binaryTree.levelOrderList}`) // [9, 5, 67, 1, 7, 23, 556, 3, 8, 27, 324, 6345, 36]
+console.log(`Tree travesed in level order is:   ${binaryTree.levelOrderList}`) // [9, 4, 67, 1, 7, 23, 556, 3, 8, 27, 324, 6345, 36]
 console.log("-------------------------------------------")
-binaryTree.preorder(); // Returns array of tree values traverseed in level order 
-console.log(`Tree travesed in preorder is: ${binaryTree.preorderList}`) // [9, 5, 67, 1, 7, 23, 556, 3, 8, 27, 324, 6345, 36]
+binaryTree.preorder(); // Returns array of tree values traverseed in preorder 
+console.log(`Tree travesed in preorder is:      ${binaryTree.preorderList}`) // [9, 4, 1, 3, 7, 8, 67, 23, 27, 36, 556, 324, 6345]
+console.log("-------------------------------------------")
+binaryTree.postorder(); // Returns array of tree values traverseed in postorder 
+console.log(`Tree travesed in postorder is:     ${binaryTree.postorderList}`) // [9, 67, 556, 6345, 324, 23, 27, 36, 4, 7, 8, 1, 3]
+
 
  
