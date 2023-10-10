11a86538d0d583ed2712ea3ee49dd82332d54efe
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-10 12:50:17 -0600

    completed levelOrderTraversal

--- computer-science/binary-search-tree/index.js
+++ computer-science/binary-search-tree/index.js
@@ -1,7 +1,7 @@
 import Tree from "./binarySearchTree.js";
 
 // Test Array
-const testArr = [1, 556, 556, 27, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testArr = [1, 556, 556, 27, 4, 23, 8, 9, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 // Construct tree from array
 const binaryTree = new Tree(testArr);
@@ -17,5 +17,8 @@ binaryTree.prettyPrint(binaryTree.root) // Removes node 5 and reorders children
 console.log("-------------------------------------------")
 console.log(binaryTree.find(27)) // Node {value: 34, left: null, right: node}
 console.log(binaryTree.find(9)) // Node {value: 34, left: Node, right: Node}
-console.log(binaryTree.depth(7)) // 3
-
+console.log(binaryTree.depth(7)) // 2
+console.log("-------------------------------------------")
+binaryTree.levelOrder(); // Returns array of tree values traverseed in level order [ ]
+console.log('halt')
+ 
