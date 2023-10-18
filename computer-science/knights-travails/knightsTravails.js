3d611c9f6ac90ab14a2f65f7cd5bc782c701ca2c
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-18 06:16:31 -0600

    initial square obj

--- computer-science/knights-travails/knightsTravails.js
+++ computer-science/knights-travails/knightsTravails.js
@@ -1,4 +1,37 @@
+// JS solution to Knights Travails problem in the Odin Projct curriculum,
+// Using JoshDevHub(https://github.com/JoshDevHub/Knight-Travails-JS/tree/main)'s method with slight modifications
 
+const squareRegistry = new Map();
+
+// Chess Square factory
+const ChessSquare = (x, y) => {
+        const xPos = x; 
+        const yPos = y;
+        let predecessor;
+
+        const OFFSETS = [
+        [1, 2], [1, -2],
+        [2, 1], [2, -1],
+        [-1, 2], [-1, -2],
+        [-2, 1], [-2, -1]
+        ];    
+
+    const getPredecessor = () => predecessor;
+    const setPredecessor = (newPred) => { predecessor ||= newPred };
+
+    // Prints xy coordinates of square
+    const name = () => `${x}, ${y}`
+
+    // Returns array of possible moves by filtering the impossibilities (null) from offset options
+    const createMoves = () => {
+        return OFFSETS.map(newSquare).filter(Boolean);
+    }
+
+    const newSquare
+
+
+
+} 
 
 knightsTravails(start, finish) {
     
