9352c3357e521882550ad8655250b8878267f374
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-23 16:49:54 -0600

    adding reverseString() test suite

--- testing/reverseString.js
+++ testing/reverseString.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-    return num
+    if (typeof str !== 'string') return null;
+
+    return str.split("").reverse().join("");
 }
+
+reverseString(8)
 
 module.exports = reverseString;
--- testing/reverseString.test.js
+++ testing/reverseString.test.js
@@ -1,5 +1,13 @@
 const reverseString = require('./reverseString');
 
-it('works', () => {
-    
-})
+test('String intake only', () => 
+    expect(reverseString(8)).toBeNull());
+
+test('Base case string reverse', () => 
+    expect(reverseString('hello')).toBe('olleh'));
+
+test('With capitals', () => 
+    expect(reverseString('Hello')).toBe('olleH'));
+
+test('With spaces and special chars', () => 
+    expect(reverseString('Hello _ Mr. ')).toBe(' .rM _ olleH'));
