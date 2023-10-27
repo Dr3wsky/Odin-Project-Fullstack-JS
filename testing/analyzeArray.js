ec1ad8b6253444065e3682abb3d60d5345849d75
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-10-27 11:57:47 -0600

    all scripts run and tests passed

--- testing/analyzeArray.js
+++ testing/analyzeArray.js
@@ -1,12 +1,25 @@
 function analyzeArray(arr) {
     // Checks the object passed is array
-    if (!Array.isArray(arr)) return null
+    if (!Array.isArray(arr)) return 'Pass an array argument'
 
-    const obj = {
-        average: undefined,
-        min: undefined,
-        max: undefined,
-        length: arr.length, 
+    // Sum numbers in array or returns null if not all items are numbers
+    let sum = 0;
+    let allNums = true;
+    arr.forEach((num) => {
+        if (typeof num !== 'number') allNums = false; 
+        else sum += num;
+    })
+    if (!allNums) return 'Array includes non-numbers';
+
+    const obj = { 
+        average: sum / arr.length,
+        min: arr.reduce((cur, prev) => {
+                return Math.min(cur, prev);
+            }),
+        max: arr.reduce((cur, prev) => {
+                return Math.max(cur, prev);
+            }),
+        len: arr.length     // Assigning len as length property as length is already a method in JS
     }
     return obj;
 }
--- testing/analyzeArray.test.js
+++ testing/analyzeArray.test.js
@@ -1,8 +1,28 @@
 const analyzeArray = require('./analyzeArray');
 
-describe('Analyze Array', () => {
-  it('Checks if arg passes is array', () => {
-    expect(analyzeArray([1, 2])).toBe(null)
+console.log(analyzeArray([5, 3, 15]))
+
+  test('Checks if arg passes is array', () => {
+    expect(analyzeArray(5)).toBe('Pass an array argument')
   });
-  
-});
+
+  test('Accepts only numbers in array', () => {
+    expect(analyzeArray([5, 3, 's'])).toBe('Array includes non-numbers')
+  });
+
+  test('Calculates average', () => {
+    expect(analyzeArray([4, 4, 4, 4]).average).toBe(4)
+  });
+
+  test('Returns length', () => {
+    expect(analyzeArray([1, 1, 1, 1, 1, 1, 1 ,1]).len).toBe(8)
+  });
+
+  test('Calculates maximum', () => {
+    expect(analyzeArray([2, 5, 10, 5]).max).toBe(10)
+  })
+
+  test('Calculates minimum', () => {
+    expect(analyzeArray([2, 5, 10, 5]).min).toBe(2)
+  })
+
